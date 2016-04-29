e6a49d4f4f25be4fb65f93ffd9f7af088d37a85a
carletti <trcarletti@gmail.com>
2016-04-29 13:57:32 -0300

    Teste de xml rpc

---
+++ xml_rpc_testes_usuário.py
@@ -0,0 +1,6 @@
+# -*- coding: utf-8 -*-
+import xmlrpc.client
+
+proxy = xmlrpc.client.ServerProxy("http://localhost:8000/")
+print("3 is even: %s" % str(proxy.is_even(3)))
+print("100 is even: %s" % str(proxy.is_even(100)))
