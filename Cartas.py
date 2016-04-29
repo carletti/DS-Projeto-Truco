1ae761ad435975109efa68ebfda58fc5d7b60b66
carletti <trcarletti@gmail.com>
2016-04-29 16:32:53 -0300

    Comecei a criar as cartas

---
+++ Cartas.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+
+class cartas():
+    
+    def __init__(self):
+        self.valor
+        self.nype
+
+baralho = [[Quatro,´Ouros´], [Quatro, ´Espada´], [Quatro, ´Copas´], [Quatro, ´Paus´], [Cinco,´Ouros´], [Cinco, ´Espada´], [Cinco, ´Copas´], [Cinco, ´Paus´], [Seis,´Ouros´], [Seis, ´Espada´], [Seis, ´Copas´], [Seis, ´Paus´], [Sete,´Ouros´], [Sete, ´Espada´], [Sete, ´Copas´], [Sete, ´Paus´], [Dama,´Ouros´], [Dama, ´Espada´], [Dama, ´Copas´], [Dama, ´Paus´], [Valete,´Ouros´], [Valete, ´Espada´], [Valete, ´Copas´], [Valete, ´Paus´], [Rei,´Ouros´], [Rei, ´Espada´], [Rei, ´Copas´], [Rei, ´Paus´], [Ás,´Ouros´], [Ás, ´Espada´], [Ás, ´Copas´], [Ás, ´Paus´]]
+print(baralho)
+len(baralho)
