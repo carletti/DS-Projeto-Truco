f5a7c45b391001ad3c78d101b41ae12b7d153b5d
carletti <trcarletti@gmail.com>
2016-05-02 15:11:51 -0300

    Terminei a lista cartas

--- Cartas.py
+++ Cartas.py
@@ -6,6 +6,45 @@ class cartas():
         self.valor
         self.nype
 
-baralho = [[Quatro,´Ouros´], [Quatro, ´Espada´], [Quatro, ´Copas´], [Quatro, ´Paus´], [Cinco,´Ouros´], [Cinco, ´Espada´], [Cinco, ´Copas´], [Cinco, ´Paus´], [Seis,´Ouros´], [Seis, ´Espada´], [Seis, ´Copas´], [Seis, ´Paus´], [Sete,´Ouros´], [Sete, ´Espada´], [Sete, ´Copas´], [Sete, ´Paus´], [Dama,´Ouros´], [Dama, ´Espada´], [Dama, ´Copas´], [Dama, ´Paus´], [Valete,´Ouros´], [Valete, ´Espada´], [Valete, ´Copas´], [Valete, ´Paus´], [Rei,´Ouros´], [Rei, ´Espada´], [Rei, ´Copas´], [Rei, ´Paus´], [Ás,´Ouros´], [Ás, ´Espada´], [Ás, ´Copas´], [Ás, ´Paus´]]
+baralho = [['Quatro','Ouros'],
+           ['Quatro', 'Espada'], 
+           ['Quatro', 'Copas'],
+           ['Quatro', 'Paus'],
+           ['Cinco','Ouros'], 
+           ['Cinco', 'Espada'],
+           ['Cinco', 'Copas'], 
+           ['Cinco', 'Paus'],
+           ['Seis','Ouros'],
+           ['Seis', 'Espada'], 
+           ['Seis', 'Copas'],
+           ['Seis', 'Paus'], 
+           ['Sete','Ouros'],
+           ['Sete', 'Espada'],
+           ['Sete', 'Copas'],
+           ['Sete', 'Paus'],
+           ['Dama','Ouros'],
+           ['Dama', 'Espada'],
+           ['Dama', 'Copas'],
+           ['Dama', 'Paus'], 
+           ['Valete','Ouros'],
+           ['Valete', 'Espada'],
+           ['Valete', 'Copas'], 
+           ['Valete', 'Paus'],
+           ['Rei','Ouros'],
+           ['Rei', 'Espada'],
+           ['Rei', 'Copas'],
+           ['Rei', 'Paus'],
+           ['Ás','Ouros'],
+           ['Ás', 'Espada'],
+           ['Ás', 'Copas'],
+           ['Ás', 'Paus'],
+           ['Dois','Ouros'],
+           ['Dois', 'Espada'], 
+           ['Dois', 'Copas'],
+           ['Dois', 'Paus'],
+           ['Três','Ouros'],
+           ['Três', 'Espada'], 
+           ['Três', 'Copas'],
+           ['Três', 'Paus']]
 print(baralho)
-len(baralho)
+print(len(baralho))
